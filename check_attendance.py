996575de59d3835a709457e5785d5d9834936830
Kacper Latuszewski <kacpx123@gmail.com>
2019-12-30 14:33:48 +0100

    conn.commit()

--- check_attendance.py
+++ check_attendance.py
@@ -5,8 +5,9 @@ import time
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 import datetime
- 
-currentDT = datetime.datetime.now()
+
+while True:
+  currentDT = datetime.datetime.now()
 
 reader = SimpleMFRC522()
 
